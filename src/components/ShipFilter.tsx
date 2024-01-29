4e3f1b978aa5b77ce943efda37d21d99c18cd62c
NicolasMarafetti <nicolas.marafetti@hotmail.fr>
2024-01-29 21:58:09 +0100

    Card 3D avec le Omen

--- src/components/ShipFilter.tsx
+++ src/components/ShipFilter.tsx
@@ -16,16 +16,16 @@ export default function ShipFilter() {
 
     return (
         <div className="flex flex-col m-auto pb-[5vh] w-40">
-            <label className="text-gray-500" htmlFor="ship_name">Ship</label>
+            <label className="mb-5 text-gray-500" htmlFor="ship_name">Ship</label>
             <ul>
                 <li>
-                    <button className="h-56">
+                    <button className="h-28 w-28">
                         <div className="card h-full w-full">
                             <div className="wrapper h-full w-full">
-                                <Image alt="ship" src="/images/Ship Filters/eve-online-card-background.webp" height="100" width="100" className="cover-image w-full" />
+                                <Image alt="ship" src="/images/Ship Filters/omen-background.webp" height="100" width="100" className="cover-image w-full" />
                             </div>
-                            <p className="absolute left-1/2 bottom-8 text-[#ebac3c] text-center text-xl title transform -translate-x-1/2 uppercase">Destroyer</p>
-                            <Image alt="ship" src="/images/Ship Filters/ship2.webp" height="300" width="300" className="character h-4/6" />
+                            <p className="absolute left-1/2 bottom-8 text-[#ebac3c] text-center text-xl title transform -translate-x-1/2 uppercase">Omen</p>
+                            <Image alt="ship" src="/images/Ship Filters/omen.webp" height="300" width="300" className="character max-h-full max-w-full" />
                         </div>
                     </button>
                 </li>
